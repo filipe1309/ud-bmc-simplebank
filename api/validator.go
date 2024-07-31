96e9cbe36329d52b69b1cd2be2d52e216da370b5
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-31 12:15:09 -0300

    feat(section-2): implement transfer money API with a custom params validator

---
+++ api/validator.go
@@ -0,0 +1,11 @@
+package api
+
+import (
+	"github.com/filipe1309/ud-bmc-simplebank/util"
+	"github.com/go-playground/validator/v10"
+)
+
+var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
+	currency := fieldLevel.Field().String()
+	return util.IsSupportedCurrency(currency)
+}

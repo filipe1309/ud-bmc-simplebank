2e4b64dcb2a2c049954b4d1457c37811fb1d9ffa
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-08-08 22:42:57 -0300

    feat: Update start.sh to run db migrations and update app.env for PostgreSQL service of docker

--- start.sh
+++ start.sh
@@ -3,7 +3,9 @@
 set -e # Exit on error
 
 echo "run db migrations"
+sed -i 's/localhost/postgres/g' /app/app.env
 source /app/app.env
+
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start app"
